fbf00f41e70f22206492f3517fe81c528ada2c89
Miguel Angel <m.escobar_27@outlook.com>
2018-10-10 15:17:54 -0400

    completando mi primer web service

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -85,6 +85,6 @@ class CategoryController extends Controller
     }
     
     public function all(){
-        return new CategoryResource(Category::all());
+        return CategoryResource::collection(Category::all());
     }
 }
